43741c3058078ece712f7073abd5a13a3ab85b6c
nuhajoy <nuhaminemnebered@gmail.com>
2025-06-28 05:39:18 +0300

    finallyyy

--- client/src/API/axiosConfig.js
+++ client/src/API/axiosConfig.js
@@ -1,10 +1,18 @@
-import axios from 'axios';
-const axiosBase = axios.create({
-  // This is for local host
-  // baseURL: 'http://localhost:5000/api',
+import axios from "axios";
 
-  // This is for hosting
-  baseURL: "https://evangadi-forum-last.onrender.com/api",
+// Create a base instance
+const axiosBase = axios.create({
+  // baseURL: "http://localhost:5000/api", // for local testing
+  baseURL: "https://evangadi-forum-last.onrender.com/api", // for production
+});
+
+// 🔐 Automatically attach token to every request
+axiosBase.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosBase;
